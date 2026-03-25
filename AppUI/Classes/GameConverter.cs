03f753c7b4965c815913242dafc487390aba6c94
ProjectXsent <25782670+ProjectXsent@users.noreply.github.com>
2026-03-25 08:18:42 +0800

    Update GameConverter.cs

--- AppUI/Classes/GameConverter.cs
+++ AppUI/Classes/GameConverter.cs
@@ -157,7 +157,7 @@ namespace AppUI.Classes
             return ret;
         }
 
-        public bool IsGamePirated()
+        /*public bool IsGamePirated()
         {
             string[] foldersToExclude = new string[] { "The_Reunion", "mods", "direct" }; // folders to skip in check
 
@@ -270,7 +270,7 @@ namespace AppUI.Classes
             }
 
             return false;
-        }
+        }*/
 
         /// <summary>
         /// Checks all files, folders, and sub-folders for signs of pirated files 
